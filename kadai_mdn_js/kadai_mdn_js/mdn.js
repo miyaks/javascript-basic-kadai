4548c5071c8ad6dcb4ceb7a25e2eebbea9c37e09
miyaks <105896227+miyaks@users.noreply.github.com>
2024-12-07 02:32:23 +0900

    課題再提出

---
+++ kadai_mdn_js/kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+var hiduke=new Date(); 
+
+
+var year = hiduke.getFullYear();
+var month = hiduke.getMonth()+1;
+var week = hiduke.getDay();
+var day = hiduke.getDate();
+
+console.log(year+"年"+month+"月"+day+"日 ");
+
